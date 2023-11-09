2c6f2a1d77b1f1a1cf35972fc40bbcac25df0a85
den <mac@macbook-pro-mac.localdomain>
2023-11-09 22:19:43 +0100

    input

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import { Header } from './components/Header/Header';
 import './App.css'
 
 function App() {
-  const invalidChars = ['e', '+', '-'];
+  const invalidChars = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiop[]/asdfghjkl;'zxcvbnm,.!@#$%^&*()_+|}{:\">?<`~\\".split('');
   return (
     <>
       <Header />
-      <input type="number" className="input" autoFocus onKeyDown={(e) => {
+      <input type="text" className="input" autoFocus onKeyDown={(e) => {
         if (invalidChars.includes(e.key)) e.preventDefault();
       }} />
     </>
