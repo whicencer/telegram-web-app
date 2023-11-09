35d21a896d75d6052ac893a45222fe5b2b795524
den <mac@macbook-pro-mac.localdomain>
2023-11-09 23:06:22 +0100

    main button on sum typing

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,12 @@
 import { Header } from './components/Header/Header';
-import './App.css'
-import { useState } from 'react';
+import { Input } from './components/Input/Input';
+import './App.css';
 
 function App() {
-  const invalidChars = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiop[]/asdfghjkl;'zxcvbnm,.!@#$%^&*()_+|}{:\">?<`~\\".split('');
-  
-  const [value, setValue] = useState('');
-
-  const changeHandler = (e) => {
-    setValue(e.target.value.replace(/[^0-9.]/g,'').replace(/\B(?=(\d{3})+(?!\d))/g, ","));
-  };
-
   return (
     <>
       <Header />
-      <input value={value} placeholder='0' type="text" className="input" maxLength={8} autoFocus onKeyDown={(e) => {
-        if (invalidChars.includes(e.key)) e.preventDefault();
-      }} onChange={changeHandler} />
+      <Input />
     </>
   );
 };
---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import './Input.css';
+import { useTelegram } from '../../hooks/useTelegram';
+
+export const Input = () => {
+	const { tg } = useTelegram();
+
+	const invalidChars = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiop[]/asdfghjkl;'zxcvbnm,.!@#$%^&*()_+|}{:\">?<`~\\".split('');
+  
+  const [value, setValue] = useState('');
+
+  const changeHandler = (e) => {
+    setValue(e.target.value.replace(/[^0-9.]/g,'').replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+  };
+
+	if (value) {
+		tg.MainButton.show();
+	}
+
+	return (
+		<input value={value} placeholder='0' type="text" className="input" maxLength={8} autoFocus onKeyDown={(e) => {
+			if (invalidChars.includes(e.key)) e.preventDefault();
+		}} onChange={changeHandler} />
+	);
+};
