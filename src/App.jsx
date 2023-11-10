d2089b17d81deae4a07175b6f5d6c5c4fc8dfaf1
den <mac@macbook-pro-mac.localdomain>
2023-11-10 22:16:14 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import { Header } from './components/Header/Header';
 import { Input } from './components/Input/Input';
-import { TonConnectButton } from '@tonconnect/ui-react';
 import './App.css';
 
 function App() {
   return (
     <>
       <Header />
-      <div className='sumInput'>
+      <div className='input'>
         <Input />
         <h2>USDT</h2>
       </div>
-      <TonConnectButton />
     </>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
-const manifest = 'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <TonConnectUIProvider manifestUrl={manifest}>
-      <App />
-    </TonConnectUIProvider>
+    <App />
   </React.StrictMode>,
 );
