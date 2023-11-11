3d21ab5fe3957b244d742ed450dc5bc07cd8691d
den <mac@macbook-pro-mac.localdomain>
2023-11-11 19:53:40 +0100

    address input with paste btn

--- src/components/CreateTransaction/CreateTransaction.jsx
+++ src/components/CreateTransaction/CreateTransaction.jsx
@@ -1,18 +1,23 @@
 import { useState } from 'react';
 import { Input } from '../Input/Input';
+import { Button } from '../Button/Button';
 import { NumberInput } from '../Input/NumberInput/NumberInput';
 import './CreateTransaction.css';
 
 export const CreateTransaction = () => {
 	const [address, setAddress] = useState('');
 	const [transactionSum, setTransactionSum] = useState('');
-	const [inputSum, setInputSum] = useState('');
+	const [clipboard, setClipboard] = useState('');
 
 	const placeholder = 'EQCSES0TZYqcVkgoguhIb8iMEo4cva...';
+	navigator.clipboard.readText().then(cliptext => setClipboard(cliptext));
 
 	return (
 		<div className='createTransaction'>
-			<Input maxLength={48} type="text" placeholder={placeholder} style={{ marginLeft: 15 }} />
+			<div style={{ display: 'flex', justifyContent: 'space-between' }}>
+				<Input maxLength={48} value={address} onChange={(e) => setAddress(e.target.value)} type="text" placeholder={placeholder} style={{ marginLeft: 15 }} />
+				<Button onClick={() => setAddress(clipboard)}>Paste</Button>
+			</div>
 			<div className='sumInput'>
         <NumberInput setNumber={setTransactionSum} />
         <h2>USDT</h2>
