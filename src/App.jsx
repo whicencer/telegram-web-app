20ae975201934a1a5f373643ddc98e984baa4a33
den <mac@macbook-pro-mac.localdomain>
2023-11-11 14:45:28 +0100

    ton payment

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
 import { Header } from './components/Header/Header';
 import { Input } from './components/Input/Input';
+import { useTonConnectUI } from '@tonconnect/ui-react';
+import { Button } from './components/Button/Button';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [tonConnectUI, setOptions] = useTonConnectUI();
+  const [currentAddress, setCurrentAddress] = useState();
+
+  const transaction = {
+    validUntil: Math.floor(Date.now() / 1000) + 600,
+    messages: [
+      {
+        // The receiver's address.
+        address: currentAddress,
+        // Amount to send in nanoTON. For example, 0.005 TON is 5000000 nanoTON.
+        amount: '2000000',
+      },
+    ]
+  }
+
   return (
     <>
       <Header />
+      <h2></h2>
       <div className='sumInput'>
         <Input />
         <h2>USDT</h2>
       </div>
+      <input type="text" onChange={e => setCurrentAddress(e.target.value)} />
+      <Button onClick={() => tonConnectUI.sendTransaction(transaction)}>Send transaction</Button>
     </>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <TonConnectUIProvider
       actionsConfiguration={{
-        twaReturnUrl: "https://t.me/denielsminiapp_bot/start"
+        twaReturnUrl: "https://t.me/denielsminiapp_bot/"
       }}
       manifestUrl='https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json'
     >
