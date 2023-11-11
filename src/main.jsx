1c934604e1b482ee62baaf1dbc228d5f7bc6bcd9
den <mac@macbook-pro-mac.localdomain>
2023-11-11 01:39:57 +0100

    TON connect button

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,12 @@ import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <TonConnectUIProvider manifestUrl='https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json'>
+    <TonConnectUIProvider
+      actionsConfiguration={{
+        twaReturnUrl: "https://t.me/denielsminiapp_bot/"
+      }}
+      manifestUrl='https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json'
+    >
       <App />
     </TonConnectUIProvider>
   </React.StrictMode>,
