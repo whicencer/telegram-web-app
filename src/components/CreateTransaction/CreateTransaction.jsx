64fbbdbb4447720d05517688f7d9ddd510b0d4d4
den <mac@macbook-pro-mac.localdomain>
2023-11-11 19:58:01 +0100

    address input with paste btn

--- src/components/CreateTransaction/CreateTransaction.jsx
+++ src/components/CreateTransaction/CreateTransaction.jsx
@@ -7,16 +7,18 @@ import './CreateTransaction.css';
 export const CreateTransaction = () => {
 	const [address, setAddress] = useState('');
 	const [transactionSum, setTransactionSum] = useState('');
-	const [clipboard, setClipboard] = useState('');
 
 	const placeholder = 'EQCSES0TZYqcVkgoguhIb8iMEo4cva...';
-	navigator.clipboard.readText().then(cliptext => setClipboard(cliptext));
+	const pasteFromClipboard = async () => {
+		const text = await navigator.clipboard.readText();
+		setAddress(text);
+	}
 
 	return (
 		<div className='createTransaction'>
 			<div style={{ display: 'flex', justifyContent: 'space-between' }}>
 				<Input maxLength={48} value={address} onChange={(e) => setAddress(e.target.value)} type="text" placeholder={placeholder} style={{ marginLeft: 15 }} />
-				<Button onClick={() => setAddress(clipboard)}>Paste</Button>
+				<Button onClick={pasteFromClipboard}>Paste</Button>
 			</div>
 			<div className='sumInput'>
         <NumberInput setNumber={setTransactionSum} />
