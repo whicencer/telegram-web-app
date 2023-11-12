bef4e0292ed94ec390baa813e28d0220dca8a206
den <mac@macbook-pro-mac.localdomain>
2023-11-12 20:41:44 +0100

    then

--- src/components/CreateTransaction/CreateTransaction.jsx
+++ src/components/CreateTransaction/CreateTransaction.jsx
@@ -21,8 +21,21 @@ export const CreateTransaction = () => {
 	};
 
 	const handleSendTransaction = () => {
-		sendTransaction(address, transactionSum, tonConnectUI.sendTransaction);
-		setTimeout(() => location.reload(), 2000);
+		const transaction = {
+			validUntil: Math.floor(Date.now() / 1000) + 600,
+			messages: [
+				{
+					// The receiver's address.
+					address: address,
+					// Amount to send in nanoTON. For example, 0.005 TON is 5000000 nanoTON.
+					amount: transactionSum*1000000000, // Amout in nanoTON
+				},
+			]
+		};
+		tonConnectUI.sendTransaction(transaction)
+			.then(() => {
+				location.reload();
+			});
 	};
 
 	if (transactionSum <= 0) {
