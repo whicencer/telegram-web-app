74f1e36f585204324861a6ad8045e8a1067a4698
den <mac@macbook-pro-mac.localdomain>
2023-11-13 23:29:07 +0100

    refactor

--- src/components/DonationLink/DonationLink.jsx
+++ src/components/DonationLink/DonationLink.jsx
@@ -23,7 +23,7 @@ export const DonationLink = () => {
 				<div className="donationLink__linkbox">
 					<span>{link}</span>
 				</div>
-				<Button onClick={copyButtonClick} style={{ backgroundColor: isCopied && '#2EFF34' }}>{ isCopied ? "Copied!" : "Copy"}</Button>
+				<Button onClick={copyButtonClick} style={{ backgroundColor: isCopied && '#29D64E' }}>{ isCopied ? "Copied!" : "Copy"}</Button>
 			</div>
 		</div>
 	);
