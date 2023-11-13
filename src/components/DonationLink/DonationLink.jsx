f3708fcd1fecc705cecb5fff4f1f5e06774f2c4b
den <mac@macbook-pro-mac.localdomain>
2023-11-13 21:16:49 +0100

    refactor

--- src/components/DonationLink/DonationLink.jsx
+++ src/components/DonationLink/DonationLink.jsx
@@ -1,15 +1,27 @@
+import { useState } from "react";
 import { Button } from "../Button/Button";
 import './DonationLink.css';
+import { useTelegram } from "../../hooks/useTelegram";
 
 export const DonationLink = () => {
+	const [link, setLink] = useState('https://t.me/bot_name/4ealrk62djks');
+	const { WebApp } = useTelegram();
+
+	const copyButtonClick = () => {
+		navigator.clipboard.writeText(link)
+			.then(() => {
+				WebApp.showAlert("Copied to clipboard!");
+			});
+	};
+
 	return (
 		<div className="donationLink">
 			<h3>Your donate link</h3>
 			<div className="donationLink__link">
 				<div className="donationLink__linkbox">
-					<span>https://t.me/bot_name/4ealrk62djks</span>
+					<span>{link}</span>
 				</div>
-				<Button>Copy</Button>
+				<Button onClick={copyButtonClick}>Copy</Button>
 			</div>
 		</div>
 	);
