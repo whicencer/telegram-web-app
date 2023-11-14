cda8a90a5214d829ca223e2b12b80ed8c197d4d7
den <mac@macbook-pro-mac.localdomain>
2023-11-15 00:24:34 +0100

    input

--- src/components/Input/NumberInput/NumberInput.jsx
+++ src/components/Input/NumberInput/NumberInput.jsx
@@ -6,13 +6,13 @@ export const NumberInput = ({setNumber}) => {
   const [value, setValue] = useState('');
 
   const changeHandler = (e) => {
-		const value = e.target.value.replace(/\B(?=(\d{3})+(?!\d))/g, " ")
+		const value = e.target.value.replace(/\B(?=(\d{3})+(?!\d))/g, " ");
     setValue(value);
 
-		setNumber(Number(value.replace(/\s/g, '')));
+		setNumber(Number(value.replace(/\s/g, '').replace(/\,/g, '.')));
   };
 
 	return (
-		<input value={value} placeholder='0' type="text" inputmode="decimal" className="input sum" maxLength={8} autoFocus onChange={changeHandler} />
+		<input value={value} placeholder='0' type="text" inputmode="decimal" className="input sum" maxLength={8} autoFocus onKeyDownonChange={changeHandler} />
 	);
 };
