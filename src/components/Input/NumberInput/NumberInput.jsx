8c607eef0bf754ce68976fa332377df337a7cdcf
den <mac@macbook-pro-mac.localdomain>
2023-11-14 23:54:36 +0100

    input

--- src/components/Input/NumberInput/NumberInput.jsx
+++ src/components/Input/NumberInput/NumberInput.jsx
@@ -2,19 +2,19 @@ import { useState } from 'react';
 import '../Input.css';
 
 export const NumberInput = ({setNumber}) => {
-	const invalidChars = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiop[]/asdfghjkl;'zxcvbnm,!@#$%^&*()_+|}{:\">?<`~\\".split('');
+	const invalidChars = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiop[]/asdfghjkl;'zxcvbnm!@#$%^&*()_+|}{:\">?<`~\\".split('');
   
   const [value, setValue] = useState('');
 
   const changeHandler = (e) => {
-		const value = e.target.value.replace(/\,/, '.').replace(/[^0-9.]/g,'').replace(/\B(?=(\d{3})+(?!\d))/g, " ")
+		const value = e.target.value.replace(/[^0-9.]/g,'').replace(/\B(?=(\d{3})+(?!\d))/g, " ")
     setValue(value);
 
 		setNumber(Number(value.replace(/\s/g, '')));
   };
 
 	return (
-		<input value={value} placeholder='0' type="text" inputmode="decimal" pattern="\d*" className="input sum" maxLength={8} autoFocus onKeyDown={(e) => {
+		<input value={value} placeholder='0' type="text" inputmode="decimal" className="input sum" maxLength={8} autoFocus onKeyDown={(e) => {
 			if (invalidChars.includes(e.key)) e.preventDefault();
 		}} onChange={changeHandler} />
 	);
