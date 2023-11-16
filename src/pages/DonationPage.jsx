64727989bab4fe88ddf0c0ccc2157cad3954905f
den <mac@macbook-pro-mac.localdomain>
2023-11-16 23:56:24 +0100

    address

--- src/pages/DonationPage.jsx
+++ src/pages/DonationPage.jsx
@@ -7,6 +7,8 @@ export const DonationPage = () => {
 	const { address } = useParams();
 	const { isAuth } = useWallet();
 
+	let realAddress = `${address[address.length-1]}:${address.substring(0, address.length-1)}`;
+
 	return (
 		<div>
 			{
@@ -14,7 +16,7 @@ export const DonationPage = () => {
 					? (
 						<>
 							<Balance />
-							<CreateTransaction address={address} />
+							<CreateTransaction address={realAddress} />
 						</>
 					)
 					: <h2>You need to authorize first</h2>
